02e7a5b0011248346341ee72a4919435635be07f
SHA-15 <saleem.hkh@outlook.com>
2024-03-25 22:29:41 +0100

    Added automatic Web browser opening upon run

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # The application running file that imports the package "website" and runs it on the local machine/server
 from website import create_app
+import webbrowser
+from threading import Timer
 
 app = create_app()
 
+def open_web_browser():
+    webbrowser.open('http://127.0.0.1:5000')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    Timer(1, open_web_browser()).start()
+    app.run()
